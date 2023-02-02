52b029796d44d8594fc1423342dd8aa30d57263f
Derek Thurn <derek@fake.email>
2023-02-02 12:59:55 -0800

    tutorial turns 2 and 3 tests

--- tests/tests/tutorial/tutorial_tests.rs
+++ tests/tests/tutorial/tutorial_tests.rs
@@ -16,7 +16,11 @@ use core_ui::actions::InterfaceAction;
 use game_data::card_name::CardName;
 use game_data::player_name::{NamedPlayer, PlayerId};
 use game_data::primitives::RoomId;
-use protos::spelldawn::{ClientRoomLocation, PlayerName};
+use protos::spelldawn::client_action::Action;
+use protos::spelldawn::{
+    ClientRoomLocation, DrawCardAction, GainManaAction, InitiateRaidAction, PlayerName,
+    RoomIdentifier,
+};
 use test_utils::client::TestSession;
 use test_utils::*;
 use user_action_data::{NamedDeck, NewGameAction, NewGameDeck, UserAction};
@@ -43,24 +47,107 @@ async fn set_up_tutorial() {
 async fn tutorial_turn_one() {
     let (game_id, user_id, _) = generate_ids();
     let mut session = new_session(game_id, user_id, OPPONENT);
-    start_tutorial(&mut session).await;
+    run_tutorial_turn_one(&mut session, user_id).await;
+}
 
-    session.play_card(
-        session.user.cards.find_in_user_hand(CardName::EldritchSurge),
-        session.user_id(),
-        None,
-    );
+async fn run_tutorial_turn_one(session: &mut TestSession, user_id: PlayerId) {
+    start_tutorial(session).await;
+
+    play_from_hand(session, CardName::EldritchSurge);
     assert!(session.user.data.toast().contains("Playing cards from your hand costs one"));
 
-    session.play_card(
-        session.user.cards.find_in_user_hand(CardName::SimpleAxe),
-        session.user_id(),
-        None,
-    );
+    play_from_hand(session, CardName::SimpleAxe);
     assert!(session.user.data.toast().contains("of the enemy's dungeon"));
 
     session.initiate_raid(RoomId::RoomA);
     assert!(session.user.data.toast().contains("To get past a defending minion"));
+
+    session.click_on(user_id, CardName::SimpleAxe.displayed_name());
+    assert!(session.user.data.toast().contains("You have accessed the room"));
+
+    click_on_score(session);
+    click_on_end_raid(session);
+
+    session.run_agent_loop().await;
+
+    assert_eq!(cards(vec![CardName::ArcaneRecovery]), session.user.cards.hand(PlayerName::User));
+    assert!(session.me().can_take_action());
+}
+
+#[tokio::test]
+async fn tutorial_cannot_raid_vault() {
+    let (game_id, user_id, _) = generate_ids();
+    let mut session = new_session(game_id, user_id, OPPONENT);
+    start_tutorial(&mut session).await;
+    let result = session.perform_action(
+        Action::InitiateRaid(InitiateRaidAction { room_id: RoomIdentifier::Vault.into() }),
+        session.user_id(),
+    );
+
+    assert!(result.is_err())
+}
+
+#[tokio::test]
+async fn tutorial_cannot_raid_sanctum() {
+    let (game_id, user_id, _) = generate_ids();
+    let mut session = new_session(game_id, user_id, OPPONENT);
+    start_tutorial(&mut session).await;
+    let result = session.perform_action(
+        Action::InitiateRaid(InitiateRaidAction { room_id: RoomIdentifier::Sanctum.into() }),
+        session.user_id(),
+    );
+
+    assert!(result.is_err())
+}
+
+#[tokio::test]
+async fn tutorial_turn_two() {
+    let (game_id, user_id, _) = generate_ids();
+    let mut session = new_session(game_id, user_id, OPPONENT);
+    run_tutorial_turn_one(&mut session, user_id).await;
+    run_tutorial_turn_two(&mut session, user_id).await;
+}
+
+async fn run_tutorial_turn_two(session: &mut TestSession, user_id: PlayerId) {
+    session.perform(Action::GainMana(GainManaAction {}), user_id);
+    play_from_hand(session, CardName::ArcaneRecovery);
+    session.perform(Action::DrawCard(DrawCardAction {}), user_id);
+    session.run_agent_loop().await;
+
+    assert_eq!(
+        cards(vec![CardName::Lodestone, CardName::SimpleHammer]),
+        session.user.cards.hand(PlayerName::User)
+    );
+    assert!(session.me().can_take_action());
+}
+
+#[tokio::test]
+async fn tutorial_turn_three() {
+    let (game_id, user_id, _) = generate_ids();
+    let mut session = new_session(game_id, user_id, OPPONENT);
+    run_tutorial_turn_one(&mut session, user_id).await;
+    run_tutorial_turn_two(&mut session, user_id).await;
+    run_tutorial_turn_three(&mut session, user_id).await;
+}
+
+async fn run_tutorial_turn_three(session: &mut TestSession, user_id: PlayerId) {
+    session.initiate_raid(RoomId::RoomA);
+    click_on_continue(session);
+    session.initiate_raid(RoomId::Vault);
+    click_on_score(session);
+    click_on_end_raid(session);
+    session.perform(Action::DrawCard(DrawCardAction {}), user_id);
+    session.run_agent_loop().await;
+
+    assert_eq!(
+        cards(vec![
+            CardName::Contemplate,
+            CardName::Lodestone,
+            CardName::SimpleClub,
+            CardName::SimpleHammer
+        ]),
+        session.user.cards.hand(PlayerName::User)
+    );
 }
 
 /// Initiates the tutorial.
@@ -82,6 +169,10 @@ async fn start_tutorial(session: &mut TestSession) {
         )
         .expect("Error starting tutorial");
     session.run_agent_loop().await
+}
+
+fn play_from_hand(session: &mut TestSession, card: CardName) {
+    session.play_card(session.user.cards.find_in_user_hand(card), session.user_id(), None);
 }
 
 fn cards(vec: Vec<CardName>) -> Vec<String> {
